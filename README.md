d8df0f357ad72c9374d5ee89c2b79cd64b275131
Leonard Krause <leoek2011@gmail.com>
2019-02-12 16:12:31 +0100

    initial fetch to curl version

---
+++ README.md
@@ -0,0 +1,33 @@
+[![MIT Licence](https://badges.frapsoft.com/os/mit/mit.svg?v=103)](https://opensource.org/licenses/mit-license.php)
+
+# fetch request to curl
+
+This module is based on [http-to-curl](https://github.com/drgx/http-to-curl). Use it to generate curl requests with the inputs you would usually use for javascripts fetch. However it does not patch any modules like http-to-curl. It is just a wrapper to generate the curl string.
+
+## Installation
+
+```sh
+yarn add fetch-to-curl
+```
+
+## Usage
+
+```js
+import fetchToCurl from 'fetch-to-curl';
+fetchToCurl();
+
+const url = 'https://jsonplaceholder.typicode.com/posts/1',
+const options = {
+  headers: {
+    Authorization: "BASIC SOMEBASE64STRING"
+  }
+  method: 'get'
+};
+//Log yopur request
+console.log(fetchToCurl(url, options));
+//Do your request
+fetch(url, options);
+
+//Output
+curl "https://jsonplaceholder.typicode.com/posts/1" -X GET -H "Authorization: BASIC SOMEBASE64STRING"
+```
---
+++ src/main.js
@@ -0,0 +1,85 @@
+/**
+ *
+ *
+ * @export
+ * @param {any} options
+ * @returns {string}
+ */
+export function generateMethod(options) {
+  const method = options.method;
+  if (!method) return '';
+  const type = {
+    GET: ' -X GET',
+    POST: ' -X POST',
+    PUT: ' -X PUT',
+    PATCH: ' -X PATCH',
+    DELETE: ' -X DELETE',
+  };
+  const methodParam = type[method.toUpperCase()];
+  return methodParam ? methodParam : '';
+}
+
+/**
+ *
+ *
+ * @export
+ * @param {any} options
+ * @returns {string}
+ */
+export function generateHeader(options) {
+  const headers = options.headers;
+  let isEncode = false;
+  if (!headers) return '';
+  let headerParam = ' ';
+  Object.keys(headers).map((val, key) => {
+    if (val.toLocaleLowerCase() !== 'content-length') {
+      headerParam += `-H "${val}: ${headers[val].replace(/(\\|")/g, '\\$1')}"`;
+    }
+    if (val.toLocaleLowerCase() === 'accept-encoding') {
+      isEncode = true;
+    }
+  });
+  return {
+    params: headerParam.trim(),
+    isEncode,
+  };
+}
+
+/**
+ *
+ *
+ * @export
+ * @param {Object} body
+ * @returns {string}
+ */
+export function generateBody(body) {
+  if (!body) return '';
+  return ` --data-binary ${JSON.stringify(body)}`;
+}
+
+/**
+ *
+ *
+ * @export
+ * @param {boolean} isEncode
+ * @return {string}
+ */
+export function generateCompress(isEncode) {
+  return isEncode ? ' --compressed' : '';
+}
+
+/**
+ *
+ *
+ * @export
+ * @param {string} url
+ * @param {Object} options
+ * @param {string} [options.body]
+ */
+export const fetchToCurl = (url, options) => {
+  const { body } = options;
+  const headers = generateHeader(options);
+  return `curl ${url}${generateMethod(options)}${headers.params}${generateBody(body)}${generateCompress(headers.isEncode)}`;
+}
+
+export default fetchToCurl;
